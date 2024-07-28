0361ffe30b642331c431ab1502287ec5762eca7c
shawn-10x <shawn-10x@users.noreply.github.com>
2024-07-28 18:09:54 +0200

    Fixes

--- middleware/admin.go
+++ middleware/admin.go
@@ -8,9 +8,11 @@ import (
 func CheckAdmin(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		adminTokenCookie, err := c.Cookie("admin_token")
+		var admin *model.Admin
 		if err == nil && adminTokenCookie != nil {
-			c.Set("admin", model.GetAdminByToken(adminTokenCookie.Value))
+			admin = model.GetAdminByToken(adminTokenCookie.Value)
 		}
+		c.Set("admin", admin)
 		return next(c)
 	}
 }
