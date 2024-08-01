a80788d245ce194bd5292b0e4cdbf5e7b86644e9
shawn-10x <shawn-10x@users.noreply.github.com>
2024-08-01 19:17:43 +0200

    Seo stuff, Router improved, Other

--- router/router.go
+++ router/router.go
@@ -1,7 +1,10 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
+	"github.com/shawn-10x/100pfps/static"
 )
 
 func New() *echo.Echo {
@@ -9,12 +12,28 @@ func New() *echo.Echo {
 
 	g := e.Group("")
 	SetupProfile(g)
-	SetupPrivacy(g)
-	SetupRules(g)
-	SetupDetails(g)
 	SetupAdmin(g)
-	SetupPublic(g)
-	SetupNotFound()
+
+	g.GET("/privacy/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "privacy.html", nil)
+	})
+	g.GET("/rules/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "rules.html", nil)
+	})
+	g.GET("/details/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "details.html", nil)
+	})
+	g.GET("/robots.txt", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "/static/robots.txt")
+	})
+	g.GET("/sitemap.xml", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "/static/sitemap.xml")
+	})
+	g.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", http.FileServer(http.FS(static.FS)))))
+
+	echo.NotFoundHandler = func(c echo.Context) error {
+		return c.Render(http.StatusNotFound, "404.html", nil)
+	}
 
 	return e
 }
